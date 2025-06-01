c562144f5eaf40c8c51ef5fb32c396cc2cbb6771
JamshidPardayev <jamshidnt2024@gmail.com>
2025-06-01 18:32:47 +0500

    hatolar tugatildi

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      ringOpacity: {
+        '50': '0.5',
+      },
+    },
+  },
+  plugins: [],
+}
